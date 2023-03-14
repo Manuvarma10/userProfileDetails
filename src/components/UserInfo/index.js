a12dfad3f7a33960863027eb4dbdfdb8bf375be3
manojvarma <manojvarma6268@gmail.com>
2023-03-14 07:15:09 +0000

    profile

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="userinfo-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="userprofile"
+    />
+    <h1 className="profilename">Wade Warren</h1>
+    <p className="profileDescription">Software developer in UK</p>
+  </div>
+)
+
+export default UserInfo
